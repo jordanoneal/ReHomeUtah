33a1bfc9d66b91ced4f368f27910e0b7224787b0
Landon Crapo <antosop@gmail.com>
2020-07-28 21:39:54 -0600

    Added .env and fixing boilerplate for typescript

--- server.ts
+++ server.ts
@@ -1,5 +1,5 @@
-const express = require("express");
-const path = require("path");
+import express from "express";
+import path from "path";
 const PORT = process.env.PORT || 3001;
 const app = express();
 
