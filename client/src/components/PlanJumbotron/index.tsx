1b9b2f869effc24ddac72a946b6840cefd71e7a8
Landon Crapo <antosop@gmail.com>
2020-09-05 18:01:49 -0600

    Build Your Plan Jumbotron chanaged and responsive

--- client/src/components/PlanJumbotron/index.tsx
+++ client/src/components/PlanJumbotron/index.tsx
@@ -1,17 +1,19 @@
 import React from "react";
-import "./style.css";
+import styles from "./style.module.css";
+import classNames from 'classnames';
 import {
     Jumbotron,
-    Container
+    Container,
 } from "reactstrap";
 
 const PlanJumbotron = () => {
     return (
-        <Jumbotron className="planJumbotron">
-            <Container>
-                <h1 className="display-3">Build Your Plan</h1>
-                <p className="lead">Let's Get Started</p>
-            </Container>
+        <Jumbotron className={styles.planJumbotron}>
+            <img src="/assets/images/manHoldingCompass.jpeg" className={styles.image}/>
+            <div className={styles.container}>
+                <h1 className={classNames("display-4", styles.header)}>Build Your Plan</h1>
+                <p className={classNames("lead", styles.started)}>Let's Get Started</p>
+            </div>
         </Jumbotron>
     )
 }
