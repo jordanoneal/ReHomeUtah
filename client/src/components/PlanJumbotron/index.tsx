a232c65d0d8833b02cb4962114d321d82757979b
Landon Crapo <antosop@gmail.com>
2020-09-05 18:04:14 -0600

    fix lint errors

--- client/src/components/PlanJumbotron/index.tsx
+++ client/src/components/PlanJumbotron/index.tsx
@@ -3,13 +3,12 @@ import styles from "./style.module.css";
 import classNames from 'classnames';
 import {
     Jumbotron,
-    Container,
 } from "reactstrap";
 
 const PlanJumbotron = () => {
     return (
         <Jumbotron className={styles.planJumbotron}>
-            <img src="/assets/images/manHoldingCompass.jpeg" className={styles.image}/>
+            <img alt="" src="/assets/images/manHoldingCompass.jpeg" className={styles.image}/>
             <div className={styles.container}>
                 <h1 className={classNames("display-4", styles.header)}>Build Your Plan</h1>
                 <p className={classNames("lead", styles.started)}>Let's Get Started</p>
